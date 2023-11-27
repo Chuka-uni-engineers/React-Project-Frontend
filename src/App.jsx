cd64d17d50de9a9ca145a91f83ba8297fb0ff643
Raccoon254 <tomsteve187@gmail.com>
2023-11-27 14:35:23 -0800

    Added Homepage

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Login from './auth/Login';
 import Register from './auth/Register';
+import Home from './home/Home';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -12,7 +13,7 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/" element={<Login/>} />
+        <Route path="/" element={<Home/>} />
       </Routes>
     </Router>
   );
