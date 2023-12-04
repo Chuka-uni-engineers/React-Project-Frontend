82e6e96c90a41bc6b8c091658ab3b043e92129d0
Evans <denziedon@gmail.com>
2023-12-04 09:26:43 +0300

    Initial commit

---
+++ src/leaders/images.jsx
@@ -0,0 +1,12 @@
+
+      const images = [
+          { name: 'Lorem Ipsum', url: 'https://i.pravatar.cc/300', description: 'Lorem ipsum dolor sit amet consectetur' },
+          { name: 'Lorem Ipsum', url: 'https://i.pravatar.cc/300', description: 'Lorem ipsum dolor sit amet consectetur' },
+          { name: 'Lorem Ipsum', url: 'https://i.pravatar.cc/300', description: 'Lorem ipsum dolor sit amet consectetur' },
+          { name: 'Lorem Ipsum', url: 'https://i.pravatar.cc/300', description: 'Lorem ipsum dolor sit amet consectetur' },
+          { name: 'Lorem Ipsum', url: 'https://i.pravatar.cc/300', description: 'Lorem ipsum dolor sit amet consectetur' },
+          { name: 'Lorem Ipsum', url: 'https://i.pravatar.cc/300', description: 'Lorem ipsum dolor sit amet consectetur' },
+          { name: 'Lorem Ipsum', url: 'https://i.pravatar.cc/300', description: 'Lorem ipsum dolor sit amet consectetur' }
+       ];
+
+       export default images;
