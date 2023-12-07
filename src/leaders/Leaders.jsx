e12ce732c9a2551acd7ffc62bb52f35503073fd6
Evans <denziedon@gmail.com>
2023-12-07 23:59:35 +0300

    Carousel component

--- src/leaders/Leaders.jsx
+++ src/leaders/Leaders.jsx
@@ -3,7 +3,7 @@ import Carousel from "./Carousel";
 
  function Leaders(){
   return(
-    <div className="border border-sky-500 m-2 h-screen max-h-max  bg-slate-400">
+    <div className="border border-sky-500 m-2 h-screen max-h-max max-w-max bg-slate-400">
        <h1 className="text-center underline h-auto w-auto">GET TO KNOW OUR LEADERS</h1>
       <Carousel/>
 
